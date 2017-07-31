a8c702e166e052a08ac0bb1159748ccdd5f557e1
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-31 07:26:46 +0100

    Update SendEmailCheckoutListener.php

--- app/Listeners/SendEmailCheckoutListener.php
+++ app/Listeners/SendEmailCheckoutListener.php
@@ -26,6 +26,7 @@ class SendEmailCheckoutListener
      * @param  CheckoutEvent  $event
      * @return void
      */
+   
     public function handle(CheckoutEvent $event)
     {
         $user = $event->getUser();
